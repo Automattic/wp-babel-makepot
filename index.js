01e847fd3cbcb8b35131fa220f656e7df720985b
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2020-02-26 17:31:06 +0200

    Remove cwd options and add base option

--- index.js
+++ index.js
@@ -8,9 +8,26 @@ const babel = require( '@babel/core' );
  */
 const defaultPreset = require( './presets' ).default;
 
-module.exports = filepath => {
+/**
+ * Merge options object with existing babel-i18n plugin options.
+ *
+ * @param   {Object} preset  Config object from extendBaseOptions.
+ * @param   {Object} options Additional options object.
+ * @returns {Object} Config object with merged options.
+ */
+const mergeOptions = ( preset, options = {} ) => {
+	const plugins = preset.plugins;
+	const babelI18nOptions = plugins[ plugins.length - 1 ][ 1 ];
+	const mergedOptions = { ...babelI18nOptions, ...options };
+
+	plugins[ plugins.length - 1 ][ 1 ] = mergedOptions;
+
+	return preset;
+};
+
+module.exports = ( filepath, options = {} ) => {
 	try {
-		return babel.transformFileSync( filepath, defaultPreset );
+		return babel.transformFileSync( filepath, mergeOptions( defaultPreset, options ) );
 	} catch ( error ) {
 		console.error( filepath, error );
 	}
