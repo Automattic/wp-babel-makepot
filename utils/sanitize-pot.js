d77f7855d5380cbb33e96ffe5956b98291a7623e
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2020-07-10 14:15:23 +0300

    Check if file exists in pot sanitize

--- utils/sanitize-pot.js
+++ utils/sanitize-pot.js
@@ -4,6 +4,10 @@
 const fs = require( 'fs' );
 
 module.exports = output => {
+	if ( ! fs.existsSync( output ) ) {
+		return;
+	}
+
 	const content = fs.readFileSync( output, 'utf8' );
 
 	fs.writeFileSync( output, content.replace( /.*#-#-#-#-#.*\n/gm, '' ) );
